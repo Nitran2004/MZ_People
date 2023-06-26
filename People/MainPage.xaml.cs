19c96a7f929bd7b169cb5503d293e76a5e63dab6
laboratorio <laboratorio@UPOAULA10212>
2023-06-26 08:56:37 -0500

    Agregar archivos de proyecto.

---
+++ People/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using People.Models;
+using System.Collections.Generic;
+using SQLite;
+
+
+namespace People;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    public async void OnNewButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        App.PersonRepo.AddNewPerson(newPerson.Text);
+        statusMessage.Text = App.PersonRepo.StatusMessage;
+    }
+
+    public async void OnGetButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        List<Person> people = App.PersonRepo.GetAllPeople();
+        peopleList.ItemsSource = people;
+    }
+
+}
+
